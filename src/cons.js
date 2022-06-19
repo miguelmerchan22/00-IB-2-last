ee6ccd258811c03093da244027e9081e9df8b6eb
joma512900 <joma51290@gmail.com>
2022-06-18 21:56:52 -0500

    Update testnet new

--- src/cons.js
+++ src/cons.js
@@ -7,12 +7,11 @@ var INFINITY = "0x9ff9fe8bbaead59eb1a7a55763dc4fd1f066e7a0"; // version 2 0xf9B9
 var TOKEN = "0x55d398326f99059fF775485246999027B3197955";
 var chainId = '0x38';
 
-/*
 if(true){// testnet comand
-    INFINITY = "0xeBbFeAc198B7bA39282fb3975051c32054F294Ae"; // version 2  0xf47fBd34E663D8F5ad78fbf46Ae097202557A8e8
+    INFINITY = "0x06Ab92e00E7479E1E4F195070Ed665fb745e00B7"; // version 2  0x06Ab92e00E7479E1E4F195070Ed665fb745e00B7
 
     TOKEN = "0xd5881b890b443be0c609BDFAdE3D8cE886cF9BAc";
     chainId = '0x61';
 }
-*/
+
 export default {proxy, WS,INFINITY, TOKEN, chainId};
